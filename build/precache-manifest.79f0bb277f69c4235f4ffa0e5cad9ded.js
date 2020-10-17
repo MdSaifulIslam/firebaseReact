c4e66371be83532b46115e914af16b7e161873f4
MdSaifulIslam <saiful12@cse.pstu.ac.bd>
2020-10-17 15:38:50 +0600

    production

---
+++ build/precache-manifest.79f0bb277f69c4235f4ffa0e5cad9ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6e565b0fa19a42477c350819ca3c9d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7b297b8c05921775b55",
+    "url": "/static/css/main.85f32031.chunk.css"
+  },
+  {
+    "revision": "134f6a48d9b1562b74f9",
+    "url": "/static/js/2.b029c8ea.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.b029c8ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7b297b8c05921775b55",
+    "url": "/static/js/main.232ae1b9.chunk.js"
+  },
+  {
+    "revision": "511196319709e31622c6",
+    "url": "/static/js/runtime-main.8794ca20.js"
+  }
+]);
